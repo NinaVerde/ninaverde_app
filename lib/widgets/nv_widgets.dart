69dd5f0484367940add2c5898fa98e44dd8463b3
Matthew Wren <matteo@nicaraguaninaverde.com>
2026-01-25 03:13:39 -0500

    fix(ci): suppress lints and fix missing import for ci pass

--- lib/widgets/nv_widgets.dart
+++ lib/widgets/nv_widgets.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart' as intl;
