e2cf5586f8d33c9070f8f96e77034639b8d1b1ac
Matthew Wren <matteo@nicaraguaninaverde.com>
2026-01-25 02:56:30 -0500

    fix: remove unused import in test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ninaverde_app/main.dart';
+
 
 void main() {
   testWidgets('App starts smoke test', (WidgetTester tester) async {
