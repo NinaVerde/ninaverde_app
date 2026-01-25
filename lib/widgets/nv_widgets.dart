c923ceabc09cd705ac36deb83ee1227d521a0cf1
Matthew Wren <matteo@nicaraguaninaverde.com>
2026-01-25 03:07:36 -0500

    fix: initialize final field in Settings and remove duplicate imports

--- lib/widgets/nv_widgets.dart
+++ lib/widgets/nv_widgets.dart
@@ -2,11 +2,9 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart' as intl;
-import 'dart:async';
 
 import '../state/app_state.dart';
 import '../services/user_prefs_service.dart';
